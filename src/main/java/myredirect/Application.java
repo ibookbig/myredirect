b0c105367affaf00b463da474125ec6787533529
ibookbig <ibookbig.cpe@gmail.com>
2019-06-13 15:46:51 +0700

    Initial commit with SpringBoot Application class

---
+++ src/main/java/myredirect/Application.java
@@ -0,0 +1,22 @@
+package myredirect;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+
+public class Application extends SpringBootServletInitializer
+{
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
